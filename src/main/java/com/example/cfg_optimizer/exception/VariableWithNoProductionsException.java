57c2148e545502198c800c77ebaf1717ebe66063
polsu <polsugi@gmail.com>
2026-04-12 01:50:46 +0200

    Changed the custom exception HTTP status

--- src/main/java/com/example/cfg_optimizer/exception/VariableWithNoProductionsException.java
+++ src/main/java/com/example/cfg_optimizer/exception/VariableWithNoProductionsException.java
@@ -3,7 +3,7 @@ package com.example.cfg_optimizer.exception;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class VariableWithNoProductionsException extends RuntimeException {
     public VariableWithNoProductionsException(String message) {
         super(message);
